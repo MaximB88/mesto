c015523b96f024b5caae4d3ed46f1f4e650743cd
Maxim Bogucharsky <bogucharskym@yandex.ru>
2020-12-26 15:30:32 +0300

    рабочая форма + исправление ошибок

--- script/index.js
+++ script/index.js
@@ -1,11 +1,11 @@
 const popup = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close-button');
-const saveButton = document.querySelector('.popup__save-button');
 const userName = document.querySelector('input[name ="user-name"]');
 const accountName = document.querySelector('.profile__account_name');
 const userInfo = document.querySelector('input[name ="user-info"]');
 const description = document.querySelector('.profile__description');
+const form = document.querySelector('.popup__container');
 
 function openPopup () {
     function getUserName() {
@@ -23,7 +23,7 @@ function closePopup () {
     popup.classList.remove('popup_opened');
 } 
 
-function handleFormSubmit (evt) {
+function handleFormSubmitOne (evt) {
     evt.preventDefault();
     accountName.textContent = userName.value;
     description.textContent = userInfo.value;
@@ -32,4 +32,4 @@ function handleFormSubmit (evt) {
 
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
-saveButton.addEventListener('submit', handleFormSubmit);
+form.addEventListener('submit', handleFormSubmitOne);
